0cd2bb0a1a05ab9f407be3a5fc906b5331ff970d
aldimahardiansyah <aldimdiansyah@gmail.com>
2021-11-17 11:45:41 +0700

    add comments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    # membuat function register
     public function register(Request $request)
     {
         /**
@@ -15,18 +16,22 @@ class AuthController extends Controller
          * 1. menerima data dari user
          * 2. memasukkan data ke database
          */
+
+        # memvalidasi request
         $request->validate([
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
+        # mengambil request
         $input = [
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ];
 
+        # menambah data pada tabel user
         $user = User::create($input);
 
         if ($user) {
@@ -35,6 +40,7 @@ class AuthController extends Controller
                 'data' => $user
             ];
 
+            # mengembalikan data user dan code 201
             return response()->json($data, 201);
         } else {
             $data = [
@@ -45,16 +51,22 @@ class AuthController extends Controller
         }
     }
 
+    # membuat function login
     public function login(Request $request)
     {
+        # memvalidasi request
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
+        # mencari user berdasarkan email
         $user = User::where('email', $request->email)->first();
+
+        # membuat token
         $token = $user->createToken('auth_token');
 
+        # membuat logika login dengan membandingkan data inputan dengan data dari database
         $isLogin = $request->email == $user->email && Hash::check($request->password, $user->password);
 
         if ($isLogin) {
@@ -63,12 +75,14 @@ class AuthController extends Controller
                 'token' => $token->plainTextToken
             ];
 
+            # mengembalikan token dan code 200
             return response()->json($data, 200);
         } else {
             $data = [
                 'message' => 'Login failed'
             ];
 
+            # mengembalikan kode 401 denga pesan login failed
             return response()->json($data, 401);
         }
     }
